7b44f78b5817d0034d901902f6b097e6b8e8f6ce
sho-takeuchi <moonlight2248@yahoo.co.jp>
2020-01-21 14:42:24 +0900

    自動更新作業完了

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,46 +1,62 @@
 $(function(){
 
-  function buildHTML(message){
-    if ( message.image ) {
-      var html =
-       `<div class=chat-main__message-list__messages=${message.id}>
-          <div class=chat-main__message-list__messages__upper-info>
-            <div class=chat-main__message-list__messages__upper-info__talker>
-              ${message.user_name}
-            </div>
-            <div class=chat-main__message-list__messages__upper-info__date>
-              ${message.created_at}
-            </div>
-          </div>
-          <div class=chat-main__message-list__messages__text>
-            <p class=chat-main__message-list__messages__text__content>
-              ${message.content}
-            </p>
-          </div>
-          <img src=${message.image} >
-        </div>`
-      return html;
-    }
-     else {
-      var html =
-       `<div class=chat-main__message-list__messages=${message.id}>
-          <div class=chat-main__message-list__messages__upper-info>
-            <div class=chat-main__message-list__messages__upper-info__talker>
-              ${message.user_name}
-            </div>
-            <div class=chat-main__message-list__messages__upper-info__date>
-              ${message.created_at}
-            </div>
-          </div>
-          <div class=chat-main__message-list__messages__text>
-            <p class=chat-main__message-list__messages__text__content>
-              ${message.content}
-            </p>
-          </div>
-        </div>`
-      return html;
+  var buildHTML = function(message) {
+    if (message.content && message.image) {
+      //data-idが反映されるようにしている
+      var html = `<div class="chat-main__message-list__messages" data-message-id=` + message.id + `>` +
+        `<div class="chat-main__message-list__messages__upper-info">` +
+          `<div class="chat-main__message-list__messages__upper-info__talker">` +
+            message.user_name +
+          `</div>` +
+          `<div class="chat-main__message-list__messages__upper-info__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="chat-main__message-list__messages__text">` +
+          `<p class="chat-main__message-list__messages__text__content">` +
+            message.content +
+          `</p>` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+    } 
+    else if (message.content) {
+      //同様に、data-idが反映されるようにしている
+      var html = `<div class="chat-main__message-list__messages" data-message-id=` + message.id + `>` +
+        `<div class="chat-main__message-list__messages__upper-info">` +
+          `<div class="chat-main__message-list__messages__upper-info__talker">` +
+            message.user_name +
+          `</div>` +
+          `<div class="chat-main__message-list__messages__upper-info__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="chat-main__message-list__messages__text">` +
+          `<p class="chat-main__message-list__messages__text__content">` +
+            message.content +
+          `</p>` +
+        `</div>` +
+      `</div>`
+    } 
+    else if (message.image) {
+      //同様に、data-idが反映されるようにしている
+      var html = `<div class="chat-main__message-list__messages" data-message-id=` + message.id + `>` +
+        `<div class="chat-main__message-list__messages__upper-info">` +
+          `<div class="chat-main__message-list__messages__upper-info__talker">` +
+            message.user_name +
+          `</div>` +
+          `<div class="chat-main__message-list__messages__upper-info__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="chat-main__message-list__messages__text">` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
     };
-  }
+    return html;
+  };  
+
   $('#new_message').on('submit', function(e){  
     e.preventDefault();
     var formData = new FormData(this);
@@ -78,10 +94,25 @@ $(function(){
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      console.log('success');
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.chat-main__message-list').append(insertHTML);
+        $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+        $("#new_message")[0].reset();
+        $("submit-btn").prop("disabled", false);
+      }
     })
     .fail(function() {
       console.log('error');
     });
   };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
