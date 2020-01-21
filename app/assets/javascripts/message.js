de2afa0b47af89dc9f436ca9278a1b297848874f
sho-takeuchi <moonlight2248@yahoo.co.jp>
2020-01-21 10:45:51 +0900

    message.js編集直前まで

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,5 @@
-$(function(){   
+$(function(){
+
   function buildHTML(message){
     if ( message.image ) {
       var html =
@@ -63,4 +64,24 @@ $(function(){
       alert("メッセージ送信に失敗しました");
      });
   });
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log('success');
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
 });
