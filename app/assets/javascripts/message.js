4c9c8c51c041b3313537e8eabe69a55e53487cc2
sho-takeuchi <moonlight2248@yahoo.co.jp>
2020-01-21 16:00:35 +0900

    修正完了

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,57 +2,54 @@ $(function(){
 
   var buildHTML = function(message) {
     if (message.content && message.image) {
-      //data-idが反映されるようにしている
-      var html = `<div class="chat-main__message-list__messages" data-message-id=` + message.id + `>` +
-        `<div class="chat-main__message-list__messages__upper-info">` +
-          `<div class="chat-main__message-list__messages__upper-info__talker">` +
-            message.user_name +
-          `</div>` +
-          `<div class="chat-main__message-list__messages__upper-info__date">` +
-            message.created_at +
-          `</div>` +
-        `</div>` +
-        `<div class="chat-main__message-list__messages__text">` +
-          `<p class="chat-main__message-list__messages__text__content">` +
-            message.content +
-          `</p>` +
-          `<img src="` + message.image + `" class="lower-message__image" >` +
-        `</div>` +
-      `</div>`
+      var html = `<div class="chat-main__message-list__messages" data-message-id=${message.id} >
+                    <div class="chat-main__message-list__messages__upper-info"> 
+                      <div class="chat-main__message-list__messages__upper-info__talker"> 
+                        ${message.user_name} 
+                      </div> 
+                      <div class="chat-main__message-list__messages__upper-info__date"> 
+                        ${message.created_at} 
+                      </div> 
+                    </div> 
+                    <div class="chat-main__message-list__messages__text"> 
+                      <p class="chat-main__message-list__messages__text__content">
+                        ${message.content} 
+                      </p> 
+                      <img src=${message.image} class="lower-message__image" >
+                    </div> 
+                  </div>`
     } 
     else if (message.content) {
-      //同様に、data-idが反映されるようにしている
-      var html = `<div class="chat-main__message-list__messages" data-message-id=` + message.id + `>` +
-        `<div class="chat-main__message-list__messages__upper-info">` +
-          `<div class="chat-main__message-list__messages__upper-info__talker">` +
-            message.user_name +
-          `</div>` +
-          `<div class="chat-main__message-list__messages__upper-info__date">` +
-            message.created_at +
-          `</div>` +
-        `</div>` +
-        `<div class="chat-main__message-list__messages__text">` +
-          `<p class="chat-main__message-list__messages__text__content">` +
-            message.content +
-          `</p>` +
-        `</div>` +
-      `</div>`
+      var html = `<div class="chat-main__message-list__messages" data-message-id=${message.id}>
+                    <div class="chat-main__message-list__messages__upper-info">
+                      <div class="chat-main__message-list__messages__upper-info__talker">
+                        ${message.user_name}
+                      </div>
+                      <div class="chat-main__message-list__messages__upper-info__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="chat-main__message-list__messages__text">
+                      <p class="chat-main__message-list__messages__text__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                  </div>`
     } 
     else if (message.image) {
-      //同様に、data-idが反映されるようにしている
-      var html = `<div class="chat-main__message-list__messages" data-message-id=` + message.id + `>` +
-        `<div class="chat-main__message-list__messages__upper-info">` +
-          `<div class="chat-main__message-list__messages__upper-info__talker">` +
-            message.user_name +
-          `</div>` +
-          `<div class="chat-main__message-list__messages__upper-info__date">` +
-            message.created_at +
-          `</div>` +
-        `</div>` +
-        `<div class="chat-main__message-list__messages__text">` +
-          `<img src="` + message.image + `" class="lower-message__image" >` +
-        `</div>` +
-      `</div>`
+      var html = `<div class="chat-main__message-list__messages" data-message-id=${message.id}>
+                    <div class="chat-main__message-list__messages__upper-info">
+                      <div class="chat-main__message-list__messages__upper-info__talker">
+                        ${message.user_name}
+                      </div>
+                      <div class="chat-main__message-list__messages__upper-info__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="chat-main__message-list__messages__text">
+                      <img src=${message.image} "class="lower-message__image">
+                    </div>
+                  </div>`
     };
     return html;
   };  
@@ -82,34 +79,25 @@ $(function(){
   });
 
   var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     last_message_id = $('.message:last').data("message-id");
     $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
       url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
       type: 'get',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
       if (messages.length !== 0) {
-        //追加するHTMLの入れ物を作る
         var insertHTML = '';
-        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
         $.each(messages, function(i, message) {
           insertHTML += buildHTML(message)
         });
-        //メッセージが入ったHTMLに、入れ物ごと追加
         $('.chat-main__message-list').append(insertHTML);
         $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
-        $("#new_message")[0].reset();
-        $("submit-btn").prop("disabled", false);
       }
     })
     .fail(function() {
-      console.log('error');
+      alert("エラーです");
     });
   };
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
