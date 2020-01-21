626ec6a07cd08cfc67bf444d8eb0c326fa003ca5
sho-takeuchi <moonlight2248@yahoo.co.jp>
2020-01-21 20:46:32 +0900

    修正しました５

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -83,7 +83,6 @@ $(function(){
 
   var reloadMessages = function() {
     last_message_id = $('.message:last').data("message-id");
-    console.log(last_message_id)
     $.ajax({
       url: "api/messages",
       type: 'get',
